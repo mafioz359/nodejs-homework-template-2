6c27651a389ec76c9527427a02df805dda601dc8
mafioz359 <mishazlupko@gmail.com>
2023-05-25 19:42:53 +0300

    create error

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -17,7 +17,7 @@ const authenticate = async (req, res, next) => {
     const user = await User.findById(id);
 
     if (!user || !user.token || user.token !== token) {
-      next(HttpError(401, "Not authorized"));
+      return next(new HttpError(401, "Not authorized"));
     }
     req.user = user;
     next();
